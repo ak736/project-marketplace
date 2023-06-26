ac36e23f1cef783b4945bb0acd0af30517633264
Krishnendu Sahoo <102577456+iKrishnendu@users.noreply.github.com>
2023-06-26 05:26:21 +0000

    Dark Mode Added

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { sidebarConstant } from "../constants/sidebar";
 
 const Sidebar = ({ handleItemClick, activeItem }) => {
   return (
-<div className="w-full h-[10vh] bottom-0 md:flex flex-col justify-around items-stretch md:w-[20%] mt-16 border bg-white dark:text-gray-100 border-r-neutral-200 fixed md:top-0 left-0 md:h-[95%] z-10">
+<div className="w-full h-[10vh] bottom-0 md:flex flex-col justify-around items-stretch md:w-[20%] mt-16 border bg-white dark:text-gray-100 dark:bg-black border-r-neutral-200 fixed md:top-0 left-0 md:h-[95%] z-10">
       <div className="px-6 md:-mt-7 flex md:flex-col gap-4 z-10 justify-around h-[10vh]">
         {sidebarConstant.map((element) => (
           <Link
