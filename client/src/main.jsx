512ad57e96861bd9669f9bad501be1bb3681d876
abrehan2 <rehannajam2@gmail.com>
2023-07-03 12:25:25 +0500

    scroll-up button

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,12 +6,14 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from "./services/store";
 import {Provider} from "react-redux"
 import axios from "axios"
+import ScrollUp from "./components/scrollBtn/ScrollUp";
 
 axios.defaults.baseURL="https://project-marketplace-kappa.vercel.app/"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
+    <ScrollUp />
     <BrowserRouter>
       <App />
     </BrowserRouter>
